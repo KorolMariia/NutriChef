deddf778914f15ac9e5af2cf74bf3502b5dff14b
Mariia Korol <marie.korol1991@gmail.com>
2023-05-02 16:01:20 +0300

    fixed store

--- src/state/store.js
+++ src/state/store.js
@@ -2,13 +2,20 @@ import { configureStore } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
 import recipesSlice from './recipes/recipesSlice';
 
-const logger = createLogger({
-  collapsed: true,
-});
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  const logger = createLogger({
+    collapsed: true,
+  });
+
+  middlewares.push(logger);
+}
 
 export const store = configureStore({
   reducer: {
     recipes: recipesSlice,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middlewares),
 });
